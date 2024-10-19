f8fab283f49ebcfe215ef237c0597b9fe83ff6b4
Sanjit-Thapa <sandipthapa383@gmail.com>
2024-10-19 23:01:39 +0545

    changes in the design of the daashboard and added the settings to the dashboard

--- public/index.php
+++ public/index.php
@@ -1,5 +1,7 @@
 
+<?php 
 
+?>
 
 
 <!DOCTYPE html>
@@ -11,11 +13,16 @@
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
-<body>
+<body class="">
     <!-- header section -->
-    <div class="bg-blue-900 flex">
-        <h1 class="text-slate-300 text-[1.8vw] p-1  font-semibold pl-3">Art Gallery</h1>
+    <div class="bg-blue-900 flex items-center justify-between">
+        <h1 class="text-slate-300 text-[1.8vw] p-1 font-semibold pl-3">Art Gallery</h1>
+        <a href="https://www.facebook.com">
+            <img src="Assets/62021a.jpg" alt="admin" class="w-16 h-14 pr-2 rounded-full ">
+        </a>
     </div>
+
+
    
     <!-- side bar section -->
     
@@ -26,30 +33,41 @@
         
         <div class="flex flex-col justify-start items-start  "> 
             <div class="m-3">
-                <button class="fa-solid fa-chart-simple text-[1.8vw] text-white p-3  hover:text-[#a3aebe] transition ease-in-out delay-10 cursor  pt-5">
-                </button> <label for="dashboard" class= "label text-white text-2xl pl-4 cursor-pointer hover:text-sky-400" >Dashboard</label>
+               <a href="https://www.facebook.com"> <button class="fa-solid fa-chart-simple text-[1.8vw] text-white p-3  hover:text-[#a3aebe] transition ease-in-out delay-10 cursor  pt-5">
+               </button></a> <label for="dashboard" class= "label text-white text-2xl pl-4 cursor-pointer hover:text-sky-400" ><a href="#">Dashboard</a></label>
             </div>
          
         <div class="m-3">
-            <button class="fa-brands fa-telegram text-[1.8vw] text-white p-3  hover:text-[#a3aebe] transition ease-in-out delay-10 cursor  pt-5">
-            </button><label for="enquiry"  class="label text-white text-2xl pl-4 cursor-pointer hover:text-sky-400 ">Enquiry</label>
+             <a href="#"> <button class="fa-brands fa-telegram text-[1.8vw] text-white p-3  hover:text-[#a3aebe] transition ease-in-out delay-10 cursor  pt-5">
+             </button></a>
+             <label for="enquiry"  class="label text-white text-2xl pl-4 cursor-pointer hover:text-sky-400 "><a href="#">Enquiry</a></label>
         </div>
         
         <div class="m-3 ">
               
-            <button class="fa-solid fa-calendar-days text-[1.8vw] text-white p-3 hover:text-[#a3aebe] transition ease-in-out delay-10 cursor  pt-5"></button> <label for="schedule" class="label text-white text-2xl cursor-pointer pl-4 hover:text-sky-400 ">Events</label>
+             <a href="#"><button class="fa-solid fa-calendar-days text-[1.8vw] text-white p-3 hover:text-[#a3aebe] transition ease-in-out delay-10 cursor  pt-5"></button></a>   
+             <label for="schedule" class="label text-white text-2xl cursor-pointer pl-4 hover:text-sky-400 "><a href="#">Events</a></label>
         </div>
 
         <div class="m-3 ">
-            <button class="fa-solid fa-palette text-[1.8vw] text-white p-3  hover:text-[#a3aebe] transition ease-in-out delay-10 cursor  pt-5">
-            </button> <label for="artist" class="label text-white text-2xl pl-4 cursor-pointer hover:text-sky-400 ">Artist</label>
+        <a href="#"><button class="fa-solid fa-palette text-[1.8vw] text-white p-3  hover:text-[#a3aebe] transition ease-in-out delay-10 cursor  pt-5">
+        </button></a> <label for="artist" class="label text-white text-2xl pl-4 cursor-pointer hover:text-sky-400 "><a href="#">Artist</a></label>
         </div>
 
         <div class="m-3 ">
-            <button class="fa-solid fa-table-cells text-[1.8vw] text-white p-3 hover:text-[#a3aebe] transition ease-in-out delay-10 cursor  pt-5"></button>
-            <label for="Art" class="label text-white text-2xl pl-4 cursor-pointer hover:text-sky-400  ">Display</label>
+            <a href="#"><button class="fa-solid fa-table-cells text-[1.8vw] text-white p-3 hover:text-[#a3aebe] transition ease-in-out delay-10 cursor  pt-5"></button></a> 
+            <label for="Art" class="label text-white text-2xl pl-4 cursor-pointer hover:text-sky-400  "><a href="#">Display</a></label>
         </div>
          
+        <div class="m-3">
+        <a href="#"><button class="fa-solid fa-gear text-[1.8vw] text-white p-3 hover:text-[#a3aebe] transition ease-in-out delay-10 cursor  pt-5"></button></a>
+       
+        
+         <label for="Setting" class="label text-white text-2xl pl-4 cursor-pointer hover:text-sky-400"><a href="#">Setting</a></label>
+
+        </div>
+
+
         </div>
        
     </div>
---
+++ public/login.php
@@ -0,0 +1,69 @@
+<?php 
+    // login for the dashboard from the single admin
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+</head>
+<body class="bg-red-400">
+    
+    <div class=" bg-white flex justify-center items-center border-2 h-[100vh] ">
+        <div class="formBody flex flex-col items-center border-2 border-red-200 min-w-[40vw] rounded-lg h-[55vh]">
+            <div class="font-bold pt-2" >
+                <h1 class="text-[2.5vw]">Admin Login</h1>
+            </div>
+                <form action="login.php" method="post" autocomplete="off" class="login flex flex-col  pt-4 gap-[1.8vw] text-[1.2vw]">
+
+            <div class="relative">
+                 <i class="fa-solid fa-user absolute left-2 top-2 flex items-center justify-center"></i>
+                <input type="email" class="border-2 w-[25vw] h-[5vh] rounded-lg border-[#230B0B] pl-[2.2rem] pb-[0.2rem] hover:placeholder:text-red-950 hover:border-indigo-600 transition duration-200" placeholder="Email" name="email" id="email" required autocomplete="off">
+            </div>
+
+                    <div class="relative">
+                        <i class="fa-solid fa-lock  absolute left-2 top-2 flex items-center justify-center"></i>
+                        <input type="password" name="password" id="password" class="border-2 w-[25vw] h-[5vh]  rounded-lg border-[#230B0B] pl-[2.2rem] pb-[0.2rem] hover:placeholder:text-red-950 hover:border-indigo-600 transition duration-200" placeholder="Password" required>
+
+                    </div>
+                  
+                    <div class="flex gap-5 justify-center pt-1 pl-5">
+                        <input type="submit" value="Login" class="border-2 cursor-pointer w-[9vw] rounded-lg bg-black text-2xl hover:border-2 hover:border-[#4723DB] transition duration-200 font-bold hover:bg-indigo-900 text-white">
+                 
+                    </div>
+
+                    <div class="flex items-center justify-center my-0.5">
+                        <div class="border-t border-black flex-grow mr-1"></div>
+                        <span class="text-black font-medium">or</span>
+                        <div class="border-t border-black flex-grow ml-2"></div>
+                    </div>
+                    
+                 </form>
+
+                 <div class="flex gap-5 pl-5">
+                    <a href="#" class=" hover:underline transition duration-200 pt-1 text-xl">Forgot Account?</a>
+               
+                </div>
+        </div>
+       
+    </div>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/gsap.min.js" integrity="sha512-7eHRwcbYkK4d9g/6tD/mhkf++eoTHwpNM9woBxtPUBWm67zeAfFC+HrdoE2GanKeocly/VxeLvIqwvCdk7qScg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script>
+        let tl =gsap.timeline();
+tl.from(".formBody",{
+    opacity:0,
+    y:-100,
+    duration:2.9,
+    ease:"elastic.out(1,0.5)"
+})
+    </script>
+</body>
+</html>
