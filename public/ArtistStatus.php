66d57861a1ef404ec5aa0a4884071bd5a965dff2
Sanjit-Thapa <sandipthapa383@gmail.com>
2024-10-30 00:31:21 +0545

    Status updation completed

--- public/ArtistStatus.php
+++ public/ArtistStatus.php
@@ -5,12 +5,56 @@
 
     if(isset($_POST['Reject']))
     {
-        echo "Rejected";
+        $Decide = $_POST['Reject'];
+        $num = $_POST['id'];
+
+        // echo "$Decide";
+        $sqlUpd = "update artistsignup_tb set Status = ? where Artist_Id = ? ";
+        // echo "The num is $num";
+        $stm = $conn->prepare($sqlUpd);
+
+        $stm->bind_param('si',$Decide,$num);
+
+        if($stm->execute()===true)
+        {
+            echo "successfully updated the status";
+            $stm->close();
+        }
+        else{
+            echo "sorry the status couldnt be updated";
+        }
     }
     else{
-        echo "hmm";
+        echo "something might be missing";
     }
 
+    // now for the Approved
+    
+    if(isset($_POST['Accept']))
+    {
+        $Decide = $_POST['Accept'];
+        $num = $_POST['id'];
+
+        // echo "$Decide";
+        $sqlUpd = "update artistsignup_tb set Status = ? where Artist_Id = ? ";
+        // echo "The num is $num";
+        $stm = $conn->prepare($sqlUpd);
+
+        $stm->bind_param('si',$Decide,$num);
+
+        if($stm->execute()===true)
+        {
+            echo "successfully updated the status";
+            $stm->close();
+        }
+        else{
+            echo "sorry the status couldnt be updated";
+        }
+    }
+    else{
+        echo "something might be missing";
+    }
+    
 ?>
 
 <!DOCTYPE html>
@@ -45,19 +89,20 @@
             while ($row = mysqli_fetch_assoc($result)) {
                 echo "<form action='ArtistStatus.php' method = 'POST' >";
                 echo "<tr class='border-2 p-2 hover:bg-gray-200' >";
-                echo "<td>" . $row['Artist_id'] . "</td>";
+                echo "<td><input type='number' name='id' value='{$row['Artist_id']}' readonly class='w-16 h-6 text-center bg-transparent border-none pointer-events-none'></td>";
+                ;
                 echo "<td>" . $row['Username'] . "</td>";
                 echo "<td>" . $row['Artistmail'] . "</td>";
                 echo "<td>" . $row['Status'] . "</td>";
                 echo "<td class='p-3'>
                 <button name='Reject' class='border-2 font-semibold hover:bg-red-700 hover:text-white 
-            rounded-full bg-red-500 text-white w-[6vw] h-[5vh] transition duration-300 ease-in-out'>
-                Reject
+            rounded-full bg-red-500 text-white w-[6vw] h-[5vh] transition duration-300 ease-in-out' value='Rejected'>
+                Rejected
              </button>
 
                  <button name='Accept' class='ml-4 border-2 font-semibold hover:bg-green-700 hover:text-white 
-                    rounded-full bg-green-500 text-white w-[6vw] h-[5vh] transition duration-300 ease-in-out'>
-                      Accept
+                    rounded-full bg-green-500 text-white w-[6vw] h-[5vh] transition duration-300 ease-in-out' value='Approved' >
+                      Approved
                  </button>
 </td>
 "; // Example button in the "Decide" column
